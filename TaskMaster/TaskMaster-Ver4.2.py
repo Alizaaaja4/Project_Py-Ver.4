bc84331dd63ced445bc6b98a014953b474998bfa
Aliza <137359388+Alizaaaja4@users.noreply.github.com>
2023-12-07 18:05:50 +0700

    revisi code

--- TaskMaster/TaskMaster-Ver4.2.py
+++ TaskMaster/TaskMaster-Ver4.2.py
@@ -10,33 +10,52 @@ root.title("TaskMaster") #memberikan nama dari aplikasi
 root.iconbitmap("icon_task.ico") #menmabah icon
 
 changefont = tkinter.font.Font(size=20)
-def app_regis():
-    judul = Label(root, text = "REGISTER", font = changefont)
-    judul.place(x=80, y = 15)
 
-    nama = Label(root, text ="Firts Name")
-    nama2 = Label(root, text="Last Name")
-    email = Label(root, text="Email")
-    username = Label(root, text="Username")
-    password = Label(root, text="Password")
+judul = Label(root, text = "REGISTER", font = changefont)
+judul.place(x=80, y = 15)
 
-    e1 = Entry(root, width=40)
-    e2 = Entry(root, width=40)
-    e3 = Entry(root, width=40)
-    e4 = Entry(root, width=40)
-    e5= Entry(root, width=40)
+labelfr = LabelFrame(root, text = "result", padx=20, pady=20)
+labelfr.place(x = 60, y=380)
 
-    nama.place(x=20, y=60)
-    nama2.place(x=20, y=100)
-    email.place(x=20, y=140)
-    username.place(x=20, y=180)
-    password.place(x=20, y=220)
+nama = Label(root, text ="Firts Name")
+nama2 = Label(root, text="Last Name")
+email = Label(root, text="Email")
+username = Label(root, text="Username")
+password = Label(root, text="Password")
 
-    e1.place(x=20, y=80)
-    e2.place(x=20, y=120)
-    e3.place(x=20, y=160)
-    e4.place(x=20, y=200)
-    e5.place(x=20, y=240)
+e1 = Entry(root, width=40)
+e2 = Entry(root, width=40)
+e3 = Entry(root, width=40)
+e4 = Entry(root, width=40)
+e5= Entry(root, width=40, show="*")
 
-    btn = Button(root, text ="Submit").place(x=120 , y=300)
+nama.place(x=20, y=60)
+nama2.place(x=20, y=100)
+email.place(x=20, y=140)
+username.place(x=20, y=180)
+password.place(x=20, y=220)
+
+e1.place(x=20, y=80)
+e2.place(x=20, y=120)
+e3.place(x=20, y=160)
+e4.place(x=20, y=200)
+e5.place(x=20, y=240)
+
+def cetak():
+    class user:
+        def __init__(self,nama,nama2,email,username,password):
+            self.nama = nama
+            self.nama2 =nama2
+            self.email = email
+            self.username = username
+            self.password = password
+        
+        def hasil(self):
+            lbl = Label(labelfr, text="Firts Name ="+self.nama+"\nLast Name ="+self.nama2+"\nEmail ="+self.email+"\nUsername ="+self.username+"\nPassword ="+self.password).grid()
+
+    ditampilkan= user(e1.get(), e2.get(), e3.get(), e4.get(), e5.get())
+    ditampilkan.hasil()
+
+btn = Button(root, text ="Submit", command=cetak).place(x=120 , y=300)
+
 root.mainloop()
